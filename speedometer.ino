e234c89cf70ae004ffee01bf7adc7596722b4a05
Rocky Quinn <raq5889@rit.edu>
2015-02-19 10:25:20 -0500

    Started to add 7-segment displays and shift registers

--- speedometer.ino
+++ speedometer.ino
@@ -19,17 +19,20 @@
   
   by Rocky Quinn
 */
+
 //HE_IN -> input from hall effect sensor
 #define HE_IN 0
 
+// currentTime & newTime is used to time out the interval of a magnet passing
+unsigned long lastTime, newTime;
+// DIAMETER is the diameter of the tire
+float DIAMETER;
 // CIRCUMFERENCE is a constant that keeps track of the circumference of a tire
 float CIRCUMFERENCE;
 // previousInput is the last input received from the hall effect sensor
 int previousInput;
-// timer is used to time out one second at a time
-int timer;
-//rate is used to keep track of how many times the magnet has done a full revolution
-int rate;
+// spd is used to keep track of the current speed
+int spd;
 
 /*
   Setup is the start up code. Only runs once.
@@ -39,10 +42,18 @@ void setup()
   Serial.begin(9600);
   while(!Serial){;}
   
-  CIRCUMFERENCE = (3.1415926535 * 31 * (1/63360));
+  DIAMETER = 31;
+  CIRCUMFERENCE = (3.1415926535 * DIAMETER / 63360);
   previousInput = 512;
-  timer = 0;
-  rate = 45;
+  spd = 00;
+  lastTime = millis();
+  newTime = lastTime;
+  
+  Serial.println("testing variables...");
+  Serial.print("Circumference = ");Serial.println(CIRCUMFERENCE);
+  Serial.print("lastTime = ");Serial.println(lastTime);
+  Serial.print("newTime = ");Serial.println(newTime);
+  Serial.println("Testing complete!\n");
 }
 
 /*
@@ -53,10 +64,8 @@ void setup()
 */
 void loop()
 {
-  timer++;
   measure_magnet();
-  if(timer == 10000)
-    calc_speed();
+  update_display(spd);
 }
 
 /*
@@ -72,24 +81,52 @@ void measure_magnet()
   if( ( (raw-previousInput) < (-100)  ||  (raw-previousInput) > 100 ) &&
          raw < 412  ||  raw > 612 )
   {
-    Serial.println("Rate increased.");
-    rate++;
+    Serial.println(" increased.");
+    newTime = millis();
+    calc_speed();
   }
   previousInput = raw;
-  //Serial.println("Raw reading: "+(String)raw);
 }
 
+
 /*
-  calc_speed gets called every second and uses rate and circumference to 
+  calc_speed gets called every second and uses  and circumference to 
   determine the speed of the wheel.
-  
-  local vars:
-    - spd = speed in miles per hour (mph)
 */
 void calc_speed()
 {
-  //Serial.println("calc_speed() called");
-  int spd = (int)(rate * CIRCUMFERENCE * 3600);
-  Serial.println(spd);
-  timer = 0;
+  if(newTime != lastTime)
+  {
+    Serial.println("Testing calc_speed()...");
+    Serial.print("spd = ");Serial.println( (CIRCUMFERENCE/( (newTime-lastTime)*3600000 )) );
+    spd = ( (int)(CIRCUMFERENCE/( (newTime-lastTime)*3600000 )) );
+    Serial.print("spd = ");Serial.println(spd);
+    Serial.println("Testing complete!\n");
+  
+    if(spd > 99)
+      spd = 99;
+    else if(spd < 0)
+      spd = 101;
+  }
+  return;
+}
+
+
+/*
+  display used to show speed or error code
+ 
+  parameters:
+    - val = value to display 
+    
+  local vars:
+    - d1 = first digit of val (Tens place)
+    - d2 = second digit of val (Ones place)
+*/
+void update_display(int val)
+{
+  //Display 00-99 for speeds
+  //Display EE for time error         (val==100)
+  //Display E1 for calc_speed errors  (val==101)
+  int d1 = val/10;
+  int d2 = val%10;
 }
